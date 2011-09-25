57892bd53cfd8b145e7788bba22106a07091fd5d
Jorge Maroto <patoroco@gmail.com>
2011-09-25 18:07:44 +0200

    Seleccionando los puntos para cortar cada letra

--- compareImages/compareImagesViewController.h
+++ compareImages/compareImagesViewController.h
@@ -8,6 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
-@interface compareImagesViewController : UIViewController
+@interface compareImagesViewController : UIViewController{
+    UIImageView *captcha;
+    UIImageView *chr1, *chr2, *chr3, *chr4, *chr5, *chr6;
+    UISlider *sliderX, *sliderWidth;
+    UILabel *labelX, *labelWidth;
+}
+    @property (nonatomic, retain) IBOutlet UIImageView *captcha;
+    @property (nonatomic, retain) IBOutlet UIImageView *chr1;
+    @property (nonatomic, retain) IBOutlet UIImageView *chr2;
+    @property (nonatomic, retain) IBOutlet UIImageView *chr3;
+    @property (nonatomic, retain) IBOutlet UIImageView *chr4;
+    @property (nonatomic, retain) IBOutlet UIImageView *chr5;
+    @property (nonatomic, retain) IBOutlet UIImageView *chr6;
+    
+    @property (nonatomic, retain) IBOutlet UISlider *sliderX;
+    @property (nonatomic, retain) IBOutlet UISlider *sliderWidth;
 
+    @property (nonatomic, retain) IBOutlet UILabel *labelX;
+    @property (nonatomic, retain) IBOutlet UILabel *labelWidth;
+
+-(UIImage *)cropImage:(UIImage *)image fromX:(float) x width:(float) width;
+-(IBAction)sliderChange:(id)sender;
+-(void)updateValueSliders;
 @end
--- compareImages/compareImagesViewController.m
+++ compareImages/compareImagesViewController.m
@@ -9,36 +9,37 @@
 #import "compareImagesViewController.h"
 
 @implementation compareImagesViewController
-
-- (void)didReceiveMemoryWarning
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
-}
+@synthesize captcha;
+@synthesize chr1, chr2, chr3, chr4, chr5, chr6;
+@synthesize sliderX, sliderWidth;
+@synthesize labelX, labelWidth;
 
 #pragma mark - View lifecycle
-
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad
-{
+- (void)viewDidLoad{
     [super viewDidLoad];
-}
-*/
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
+    [self updateValueSliders];
+    [captcha setImage:[UIImage imageNamed:@"captcha.jpg"]];
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+-(UIImage *)cropImage:(UIImage *)image fromX:(float) x width:(float) width{
+    CGRect cropRect = CGRectMake(x, 0, width, image.size.height);
+    CGImageRef imageRef = CGImageCreateWithImageInRect([image  CGImage], cropRect);
+    UIImage *newImage = [[UIImage alloc] initWithCGImage:imageRef scale:1.0 orientation:image.imageOrientation];
+    CGImageRelease(imageRef);
+    return newImage;
+}
+
+-(IBAction)sliderChange:(id)sender{
+    [self updateValueSliders];
+    UIImage *image = [self cropImage:captcha.image fromX:sliderX.value width:sliderWidth.value];
+    [chr1 setImage:image];
+    [chr1 setFrame:CGRectMake(chr1.frame.origin.x, chr1.frame.origin.y, image.size.width*2, image.size.height*2)];
+    [image release];
+}
+
+-(void)updateValueSliders{
+    labelX.text = [NSString stringWithFormat:@"%d", (int)sliderX.value];
+    labelWidth.text = [NSString stringWithFormat:@"%d", (int)sliderWidth.value];
 }
 
 @end
